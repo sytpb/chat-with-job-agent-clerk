cd13caa3e088f47ab491b131dfef678c03e6e3ec
sytpb <songyantao@foxmail.com>
2023-11-24 22:59:37 +0200

    clean unused code

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -58,31 +58,13 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
             p({ children }) {
               return <p className="mb-2 last:mb-0">{children}</p>
             },
-            code({ node, inline, className, children, ...props }) {
-              if (children.length) {
-                if (children[0] == '▍') {
-                  return (
-                    <span className="mt-1 animate-pulse cursor-default">▍</span>
-                  )
-                }
-
-                children[0] = (children[0] as string).replace('`▍`', '▍')
-              }
-
+            code({ node, className, children, ...props }) {
               const match = /language-(\w+)/.exec(className || '')
-
-              if (inline) {
-                return (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                )
-              }
 
               return (
                 <CodeBlock
                   key={Math.random()}
-                  language={(match && match[1]) || ''}
+                  language={!!match ? match[1] : ''}
                   value={String(children).replace(/\n$/, '')}
                   {...props}
                 />
